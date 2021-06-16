590e815e86dfae10866fcc025429bb334db49fdf
Rishav Das <rdofficial192@gmail.com>
2021-06-16 09:27:52 +0530

    Updated the modules/encryption.py module file. Added the code for complete functionality of the 'ImageEncrypter' class.

--- python-shell/modules/encryption.py
+++ python-shell/modules/encryption.py
@@ -7,10 +7,10 @@ Author : Rishav Das (https://github.com/rdofficial/)
 Created on : June 13, 2021
 
 Last modified by : Rishav Das (https://github.com/rdofficial/)
-Last modified on : June 15, 2021
+Last modified on : June 16, 2021
 
 Changes made in the last modification :
-1. Created the 'ImageEncrypter' and the 'VideoEncrypter' class.
+1. Added the code for complete functionality of the 'ImageEncrypter' class.
 
 Authors contributed to this script (Add your name below if you have contributed) :
 1. Rishav Das (github:https://github.com/rdofficial/, email:rdofficial192@gmail.com)
@@ -18,7 +18,7 @@ Authors contributed to this script (Add your name below if you have contributed)
 
 # Importing the required functions and modules
 try:
-	from base64 import b64encode, b64decode
+	from base64 import b64encode, b64decode, decodebytes
 	from io import TextIOWrapper
 	from os import path
 	import hashlib
@@ -568,20 +568,297 @@ class FileEncrypter:
 class ImageEncrypter:
 	""" """
 
-	def __init__(self):
-		pass
+	def __init__(self,filename = None, password = None, arguments = None):
+		# Checking if arguments provided or just the parameters directly
+		if arguments == None:
+			# If the arguments are not passed to this function by the user, then we continue to use the default provided values
+
+			self.filename = filename
+			self.password = password
+		else:
+			# If the arguments are passed to this function by the user, then we continue to parse the arguments
+
+			# Parsing the arguments entered to this function
+			# ----
+			# Setting the default value of the variables to None
+			self.filename = None
+			self.password = None
+			self.task = None
+			self.documentation = False  # Setting the documentation flag class variable to False by default
+
+			# Iterating through each argument to filter out the values
+			for index, argument in enumerate(arguments):
+				# Iterating through each argument item
+
+				if argument == '--password':
+					# If the argument is for specifying the password, then we continue to parse the next argument as the entered value
+
+					try:
+						self.password = arguments[index + 1]
+					except IndexError:
+						# If the next argument is out of the list index (i.e., it does not exists), then we continue for the next iteration
+
+						continue
+
+				if argument == '--file':
+					# If the argument is for specifying the file, then we continue to parse the next argument as the entered value
+
+					try:
+						self.filename = arguments[index + 1]
+					except IndexError:
+						# If the next argument is out of the list index (i.e., it does not exists), then we continue for the next iteration
+
+						continue
+
+				if argument == '--task':
+					# If the argument is for specifying the task (encryption / decryption), then we continue to parse the next argument as the entered value
+
+					try:
+						self.task = arguments[index + 1]
+					except IndexError:
+						# If the next argument is out of the list index (i.e., it does not exists), then we continue for the next iteration
+
+						continue
+
+				if argument == '--help':
+					# If the argument is for specifying the help, then we continue to mark the documentation mode to be true
+
+					self.documentation = True
+			# ----
+
+			# Checking whether the task is to be in documentation mode or execution mode
+			if self.documentation:
+				# If the user specified the documentation mode, then we continue to display the help text on the console screen
+
+				print('<-- Help for ImageEncrypter -->')
+			else:
+				# If the user specified the execution mode, then we continue to execute the task
+
+				# Validating the user entered parameters
+				# ----
+				# Validating the filename parameter input (The name / location of the file that is to be encrypted using this tool)
+				if self.filename == None:
+					# If the filename parameter is not specified by the user (default), then we raise an error with a custom message
+
+					raise SyntaxError('Image file name not specified.')
+				else:
+					# If the filename parameter is specified by the user, then we continue for further validation
+
+					if type(self.filename) == str:
+						# If the filename parameter input specified by the user is of str type, then we continue
+
+						if path.isfile(self.filename):
+							# If the file specified by the user does exists, then we continue
+
+							if self.filename[len(self.filename) - 3:] == 'jpg' or self.filename[len(self.filename) - 3:] == 'jpeg' or self.filename[len(self.filename) - 3:] == 'png':
+								# If the image file specified by the user has the extension JPG or PNG, then we continue
+
+								pass
+							else:
+								# If the image file specified by the user does not have the extension JPG or PNG, then we raise an error with a custom message
+
+								raise TypeError('Image file specified is not of valid extension. Accepted are jpg or png.')
+						else:
+							# If the file specified by the user does not exists, then we raise an error with a custom message
+
+							raise FileNotFoundError('Specified image file does not exists.')
+					else:
+						# If the filename parameter input specified by the user is not of str type, then we raise an error on the console screen
+
+						raise TypeError('Image file name parameter should be in str format.')
+
+				# Validating the password parameter entered by the user
+				if self.password == None:
+					# If the password parameter is not specified by the user (default), then we raise an error with a custom message
+
+					raise SyntaxError('Password not specified.')
+				else:
+					# If the password parameter is specified by the user, then we continue for further validation
+
+					if type(self.password) == str:
+						# If the password parameter input specified by the user is of str type, then we continue to validate further
+
+						if len(self.password) > 4:
+							# If the password parameter input specified by the user is more than 4 character length, then we continue
+
+							pass
+						else:
+							# If the password parameter input specified by the user is less than 4 character length, then we raise an error with a custom message
+
+							raise SyntaxError('Password invalid. The password input should be a string with atleast 5 character length.')
+					else:
+						# If the password parameter input specified by the user is not of str type, then we raise an error on the console screen
+
+						raise TypeError('Password invalid. Password parameter should be in str format.')
+				# ----
+
+				# Checking the task specified and then continuing to execute the task
+				if self.task == None:
+					# If the task to be done is not specified by the user (default value), then we raise an error with a custom message
+
+					raise SyntaxError('Task not specified. The task is needed to be specified whether encrypt / decrypt.')
+				elif self.task.lower() == 'encrypt' or self.task.lower() == 'encryption':
+					# If the task specified is for encryption, then we continue to encrypt
+
+					if self.encrypt() == 0:
+						# If the encrypt() method returns 0, then the file has been encrypted successfully and we display the success message on the console screen
+
+						print(f'[ Image encrypted : {self.filename} ]')
+					else:
+						# If the encrypt() method does not returns 0, then the file has been failed to encrypt and we display the failure message on the console screen
+
+						print(f'[ Image failed to encrypt : {self.filename} ]')
+				elif self.task.lower() == 'decrypt' or self.task.lower() == 'decryption':
+					# If the task specified is for decryption, then we continue to decrypt
+
+					if self.decrypt() == 0:
+						# If the decrypt() method returns 0, then the file has been decrypted successfully and we display the success message on the console screen
+
+						print(f'[ Image file decrypted : {self.filename} ]')
+					else:
+						# If the decrypt() method does not returns 0, then the file has been failed to decrypt and we display the failure message on the console screen
+
+						print(f'[ Image file failed to decrypt : {self.filename} ]')
+				else:
+					# If the task specified is not recognized, then we raise an error with a custom message
+
+					raise ReferenceError('Task not recognized. Encrypt / decrypt are the two recognizable terms.')
+				del self.task
 
 	def generatekey(self):
-		pass
+		""" This method / function serves the purpose of generating a special key for the encryption and decryption using the user entered password. This function takes the value of the user entered password from the class variable self.password.
+		The key is generated in such an algorithm, that the key remains possitive integer.
+
+		This function returns the int format key back after the generation. """
+
+		# Generating the key from the encryption using the user entered password for encryption / decryption
+		key = 0
+		isEven = True
+
+		for i in self.password:
+			# Iterating over each character in the encrypted key entered by the user
+				
+			if isEven:
+				# If the current iteration is even number, then we add the char code value
+
+				key += ord(i)
+			else:
+				# If the current iteration is odd number (not even), then we subtract the char code value
+
+				key -= ord(i)
+		del isEven
+
+		# Making the key possitive
+		if key < 0:
+			# If the key value is less than 0, then we change the negative sign to possitive by simply multiplying it with -1
+
+			key *= (-1)
+
+		# Adding the length of the password to itself
+		key += len(self.password)
+
+		# Returning the generating key
+		return key
 
 	def checkpassword(self):
-		pass
+		""" This method / function serves the functionality of checking the user entered password against the original password stored in the image file. This function uses the value of password from the class variable self.password.
+
+		When a image file is encrypted, the original password is also saved along with the encrypted contents of the image file. Thus, we can verify the password of the file before decrypting the file. The function returns True if the password matches, and returns False if the password does not matches. """
+
+		# Reading the contents of the image file
+		contents = open(self.filename, 'rb').read()
+		contents = contents.decode()
+
+		# Reading the password stored in the first line
+		password = contents.split('\n')[0]
+		del contents
+
+		# Checking the user entered password against the original password stored in the file
+		if hashlib.md5(self.password.encode()).hexdigest() == password:
+			# If the hashes of user entered password and the original password matches, then we return True
+
+			return True
+		else:
+			# If the hashes of user entered password and the original password matches, then we return False
+
+			return False
 
 	def encrypt(self):
-		pass
+		""" This method / function serves the feature of encrypting the contents of the image file. This function reads the filename location input stored in the class variable self.filename. """
+
+		# Reading the contents of the image file specified by the user
+		contents = open(self.filename, 'rb').read()
+		contents = b64encode(contents).decode()
+
+		# Generating the encryption key
+		key = self.generatekey()
+
+		# Converting the plain text to cipher text
+		text = ''
+		for character in contents:
+			# Iterating through each character
+
+			text += chr((ord(character) + key) % 256)
+
+		# Changing the encoding of the content to base64 format
+		text = b64encode(text.encode()).decode()
+
+		# Adding the password to the contents of the image file
+		text = hashlib.md5(self.password.encode()).hexdigest() + '\n' + text
+
+		# Saving the encrypted content back to the file
+		open(self.filename, 'wb').write(text.encode())
+
+		# Deleting some of the variables declared within this function
+		del text, key, contents
+
+		# Returning 0 code (It will indicate that the function executed in success)
+		return 0
 
 	def decrypt(self):
-		pass
+		""" This method / function serves the feature of decrypting the contents of the image file. This function reads the filename location input stored in the class variable self.filename. """
+
+		# Validating the password before decrypting
+		if self.checkpassword():
+			# If the password matches, then we continue
+
+			# Reading the contents of the image file specified by the user
+			contents = open(self.filename, 'rb').read()
+
+			# Removing the password part from the file
+			contents = contents.decode()
+			contents = ' '.join(contents.split('\n')[1:])
+
+			# Converting the base64 format text to plain text
+			contents = b64decode(contents.encode()).decode()
+
+			# Generating the encryption key
+			key = self.generatekey()
+
+			# Converting the cipher text to plain text
+			text = ''
+			for character in contents:
+				# Iterating through each character
+
+				text += chr((ord(character) - key) % 256)
+
+			# Converting the contents to image file content type
+			text = text.encode()
+			text = decodebytes(text)
+
+			# Saving the decrypted content back to the image file
+			open(self.filename, 'wb').write(text)
+
+			# Deleting some of the variables declared within this function
+			del text, key, contents
+
+			# Returning 0 code (It will indicate that the function executed in success)
+			return 0
+		else:
+			# If the password does not matches, then we display the error message on the console screen
+
+			print(f'[ Incorrect password ]')
+			return 403
 
 class VideoEncrypter:
 	""" """
