7666d5684ccf718a111ca9254dbcb40d305a0c29
Rishav Das <rdofficial192@gmail.com>
2021-06-18 09:04:03 +0530

    Created the modules/image.py module file.

---
+++ python-shell/modules/image.py
@@ -0,0 +1,14 @@
+"""
+image.py - Python Shell
+
+A module created for supplying the required functions to the shell application. The functions and classes defined in this module ease many tasks at the shell application, in general way they are used in executing the task assigned by the user via the commands. This module contains functions and classes related to directory handling and other such stuff.
+
+Author : Rishav Das (https://github.com/rdofficial/)
+Created on : June 18, 2021
+
+Last modified by : -
+Last modified on : -
+
+Authors contributed to this script (Add your name below if you have contributed) :
+1. Rishav Das (github:https://github.com/rdofficial/, email:rdofficial192@gmail.com)
+"""
