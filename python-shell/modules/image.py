5f5d8622eade81734cd117a6798eab1b931d5051
Rishav Das <rdofficial192@gmail.com>
2021-06-18 09:08:18 +0530

    Updated the modules/image.py module file. Created the ImageDetails, ImageResizer, ImageRawConverter for serving their respective functionability.

--- python-shell/modules/image.py
+++ python-shell/modules/image.py
@@ -6,9 +6,51 @@ A module created for supplying the required functions to the shell application. 
 Author : Rishav Das (https://github.com/rdofficial/)
 Created on : June 18, 2021
 
-Last modified by : -
-Last modified on : -
+Last modified by : Rishav Das (https://github.com/rdofficial/)
+Last modified on : June 18, 2021
+
+Changes made in the last modification :
+1. Created the ImageDetails, ImageResizer, ImageRawConverter for serving their respective functionability.
 
 Authors contributed to this script (Add your name below if you have contributed) :
 1. Rishav Das (github:https://github.com/rdofficial/, email:rdofficial192@gmail.com)
 """
+
+class ImageDetails:
+	""" """
+
+	def __init__():
+		pass
+
+	def getimgdetails(self):
+		pass
+
+class ImageResizer:
+	""" """
+
+	def __init__(self):
+		pass
+
+	def resize(self):
+		pass
+
+	def compress(self):
+		pass
+
+	def getimgdetails(self):
+		pass
+
+class ImageRawConverter:
+	""" """
+
+	def __init__(self):
+		pass
+
+	def makeraw(self):
+		pass
+
+	def makeimage(self):
+		pass
+
+	def getimgdetails(self):
+		pass
