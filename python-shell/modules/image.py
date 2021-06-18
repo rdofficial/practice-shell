ac917fb451e425e0b2020898fcfb09266f0ff3ab
Rishav Das <rdofficial192@gmail.com>
2021-06-18 09:10:54 +0530

    Updated the modules/image.py module file. Added the code for importing all the required modules (the standard python module that are required for all the functions and classes defined in this file to be run properly without any errors).

--- python-shell/modules/image.py
+++ python-shell/modules/image.py
@@ -10,11 +10,22 @@ Last modified by : Rishav Das (https://github.com/rdofficial/)
 Last modified on : June 18, 2021
 
 Changes made in the last modification :
-1. Created the ImageDetails, ImageResizer, ImageRawConverter for serving their respective functionability.
+1. Added the code for importing all the required modules (the standard python module that are required for all the functions and classes defined in this file to be run properly without any errors).
 
 Authors contributed to this script (Add your name below if you have contributed) :
 1. Rishav Das (github:https://github.com/rdofficial/, email:rdofficial192@gmail.com)
 """
+
+# Importing the required functions and modules
+try:
+	from base64 import b64encode, b64decode, decodebytes
+	from io import TextIOWrapper
+	from os import stat, path
+except Exception as e:
+	# If there are any errors during the importing of the modules, then we display the error on the console screen
+
+	input(f'\n[ Error : {e} ]\nPress enter key to continue...')
+	exit()
 
 class ImageDetails:
 	""" """
